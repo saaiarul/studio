9b2a9abb65beb8ed943ba7ae95f2299a42eeae8d
Saai arul prakaash <saaianbu2004@gmail.com>
2025-08-09 08:48:51 +0000

    hardcode them '

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = "https://ttuzibwedktfttqoqwwh.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0dXppYndlZGt0ZnR0cW9xd3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3Mjc0MzYsImV4cCI6MjA3MDMwMzQzNn0.saOCtqwode0dGDGLcQpPQxKyov1mAVV9jVjsbOhVOhY"
 
 if (!supabaseUrl) {
     throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
